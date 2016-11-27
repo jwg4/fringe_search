79901c381bd4a3e7331829d1bb93fac622397730
jwg4 <jack.grahl@yahoo.co.uk>
2016-11-27 01:38:38 +0000

    Add a function to make a fringe from actual data

--- src/fringe.h
+++ src/fringe.h
@@ -1,3 +1,18 @@
+#include <stdint.h>
+
+typedef int64_t Fringe;
+typedef int16_t Row;
+
+inline Fringe make_fringe(Row a, Row b, Row c, int16_t modulus, int16_t first_empty) {
+  return (
+    (a << 1) &
+    (b << (WIDTH + 2)) &
+    (c << (WIDTH * 2 + 3)) &
+    (modulus << (WIDTH * 3 + PADDING_BIT_WIDTH)) &
+    (first_empty << (WIDTH * 3 + PADDING_BIT_WIDTH + MODULUS_BIT_WIDTH))
+  );
+}
+
 Fringe add_T_1(Fringe);
 Fringe add_T_2(Fringe);
 Fringe add_T_2(Fringe);
