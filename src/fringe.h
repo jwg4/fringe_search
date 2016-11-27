7ec682c630ba5b20e5a89ac65bce2e06350f2129
jwg4 <jack.grahl@yahoo.co.uk>
2016-11-27 02:45:42 +0000

    Do casts and add missing ;

--- src/fringe.h
+++ src/fringe.h
@@ -7,11 +7,11 @@ typedef int16_t Row;
 
 inline Fringe make_fringe(Row a, Row b, Row c, int16_t modulus, int16_t first_empty) {
   return (
-    (a << 1) &
-    (b << (WIDTH + 2)) &
-    (c << (WIDTH * 2 + 3)) &
-    (modulus << (CELL_BIT_WIDTH + PADDING_BIT_WIDTH)) &
-    (first_empty << (CELL_BIT_WIDTH + PADDING_BIT_WIDTH + MODULUS_BIT_WIDTH))
+    ((int64_t)a << 1) &
+    ((int64_t)b << (WIDTH + 2)) &
+    ((int64_t)c << (WIDTH * 2 + 3)) &
+    ((int64_t)modulus << (CELL_BIT_WIDTH + PADDING_BIT_WIDTH)) &
+    ((int64_t)first_empty << (CELL_BIT_WIDTH + PADDING_BIT_WIDTH + MODULUS_BIT_WIDTH))
   );
 }
 
@@ -28,7 +28,7 @@ Fringe overflow_mask = (
   & (1 << (WIDTH + 1))
   & (1 << (2 * WIDTH + 2))
   & (1 << (3 * WIDTH + 3))
-)
+);
 
 Fringe add_T_1(Fringe);
 Fringe add_T_2(Fringe);
