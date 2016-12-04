f754ab74364f78fad6d00ae474d918820abf793e
jwg4 <jack.grahl@yahoo.co.uk>
2016-12-04 23:50:29 +0000

    Add some missing type declarations

--- src/fringe.cpp
+++ src/fringe.cpp
@@ -12,7 +12,7 @@ inline Fringe add_some_T(Fringe current, int64_t piece, int16_t height, char che
   Fringe added = current | piece;
   int16_t filled = first_empty + height;
   if (filled == WIDTH){
-    modulus = (current & MODULUS_MASK) >> MODULUS_OFFSET;
+    int16_t modulus = (current & MODULUS_MASK) >> MODULUS_OFFSET;
     current = (current >> (WIDTH * 2 + 2)) & FIRST_TWO_ROWS_MASK;
     modulus = (modulus + 1) % MODULUS;
     current = current | (modulus << MODULUS_OFFSET);
@@ -23,21 +23,21 @@ inline Fringe add_some_T(Fringe current, int64_t piece, int16_t height, char che
 }
 
 Fringe add_T_1(Fringe current){
-  const piece = make_piece(14, 4, 0);
+  const int64_T piece = make_piece(14, 4, 0);
   return add_some_T(current, piece, 3, 0);
 }
 
 Fringe add_T_2(Fringe current){
-  const piece = make_piece(2, 3, 2);
+  const int64_t piece = make_piece(2, 3, 2);
   return add_some_T(current, piece, 1, 1);
 }
 
 Fringe add_T_3(Fringe current){
-  const piece = make_piece(2, 7, 0);
+  const int64_t piece = make_piece(2, 7, 0);
   return add_some_T(current, piece, 1, 0);
 }
 
 Fringe add_T_4(Fringe current){
-  const piece = make_piece(2, 6, 2);
+  const int64_t piece = make_piece(2, 6, 2);
   return add_some_T(current, piece, 1, 0);
 }
