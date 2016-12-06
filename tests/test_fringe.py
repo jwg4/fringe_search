69759296ae546839c89cf0e86fe2de5fabf06fe1
jwg4 <jack.grahl@yahoo.co.uk>
2016-12-06 23:07:29 +0000

    Correct name of object

--- tests/test_fringe.py
+++ tests/test_fringe.py
@@ -3,7 +3,7 @@ import ctypes
 
 class TestBranch(unittest.TestCase):
     def setUpClass(cls):
-        cls.obj = ctypes.CDLL("obj/fringe13.o")
+        cls.obj = ctypes.CDLL("obj/fringe_13.o")
         
         def test_object(self):
             self.assertIsNotNone(self.obj)
